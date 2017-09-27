8fcf25ddfdebc96622d8062e2cd246c2510f907d
panpan <panp85@163.com>
2017-09-27 11:33:48 +0800

    first commit

---
+++ Spydroid/build/generated/source/buildConfig/debug/net/majorkernelpanic/spydroid/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package net.majorkernelpanic.spydroid;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "net.majorkernelpanic.spydroid";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1000;
+  public static final String VERSION_NAME = "10";
+}
